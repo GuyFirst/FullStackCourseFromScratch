b4a694ed6f03d4453c51b2ae2990061802a948c9
Guy First <guyfi2@mta.ac.il>
2025-05-05 11:51:29 +0300

    little work

--- Dice Game/Dicee+Challenge+-+Starting+Files/index.js
+++ Dice Game/Dicee+Challenge+-+Starting+Files/index.js
@@ -1,2 +1,9 @@
-var randomNumber = Math.floor((Math.random() * 6)) + 1
-console.log(randomNumber)
+
+	var randomNumber = Math.floor((Math.random() * 6)) + 1;
+
+	var randomDiceImage = "dice" + randomNumber + ".png";
+
+    var randomImageSource = "images/" + randomDiceImage; 
+
+	document.querySelectorAll("img")[0].setAttribute("src", randomImageSource);
+
